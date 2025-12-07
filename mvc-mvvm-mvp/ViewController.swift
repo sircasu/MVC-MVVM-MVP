735f4171709955b2fd51d8ba1a5139899a3de556
Matteo Casu <matteo.casu91@gmail.com>
2025-12-07 10:59:40 +0100

    Initial Commit

---
+++ mvc-mvvm-mvp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvc-mvvm-mvp
+//
+//  Created by Matteo Casu on 07/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mvc-mvvm-mvpUITests/mvc_mvvm_mvpUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  mvc_mvvm_mvpUITestsLaunchTests.swift
+//  mvc-mvvm-mvpUITests
+//
+//  Created by Matteo Casu on 07/12/25.
+//
+
+import XCTest
+
+final class mvc_mvvm_mvpUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
