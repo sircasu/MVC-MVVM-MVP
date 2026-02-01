78543cc42d1b566b2bc350c38b0d586dd0b98fa7
Matteo Casu <matteo.casu91@gmail.com>
2026-02-01 18:44:46 +0100

    `RemoteProductImageDataLoader.loadImageData` delivers error on client error

--- Core/CoreTests/API/RemoteProductImageDataLoaderTests.swift
+++ Core/CoreTests/API/RemoteProductImageDataLoaderTests.swift
@@ -6,9 +6,28 @@
 //
 
 import XCTest
+import Core
 
 final class RemoteProductImageDataLoader {
-    init(client: Any) {}
+    
+    let client: HTTPClientSpy
+    
+    init(client: HTTPClientSpy) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (ProductImageLoader.Result) -> Void) {
+        let urlRequest = URLRequest(url: url)
+        client.perform(urlRequest) { result in
+        
+            switch result {
+            case .success : break
+            case let .failure(error):
+                completion(.failure(error))
+            }
+
+        }
+    }
 }
 
 final class RemoteProductImageDataLoaderTests: XCTestCase {
@@ -17,7 +36,43 @@ final class RemoteProductImageDataLoaderTests: XCTestCase {
         
         let (_, client) = makeSUT()
         
-        XCTAssertEqual(client.requestedURLs, 0)
+        XCTAssertEqual(client.requests.count, 0)
+    }
+        
+    
+    func test_loadImageDataFromURL_requestsDataFromURL() {
+        
+        let (sut, client) = makeSUT()
+        
+        sut.loadImageData(from: anyURL()) { _ in }
+        
+        XCTAssertEqual(client.requests.count, 1)
+    }        
+    
+    
+    func test_loadImageDataFromURL_deliversErrorOnClientError() {
+        
+        let (sut, client) = makeSUT()
+        
+        let error = anyNSError()
+        
+        let exp = expectation(description: "Wait for completion")
+        var receivedError: NSError?
+        sut.loadImageData(from: anyURL()) { result in
+           
+            switch result {
+            case let .failure(error):
+                receivedError = error as NSError
+            default: XCTFail("Expected error got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        client.completeWithError(anyNSError())
+        
+        wait(for: [exp], timeout: 1)
+
+        XCTAssertEqual(receivedError, error)
     }
     
     // MARK: - Helpers
