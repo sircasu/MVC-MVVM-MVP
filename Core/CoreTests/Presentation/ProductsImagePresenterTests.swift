7576bf045c5bb972a026ba00b8466576fa26ed80
Matteo Casu <matteo.casu91@gmail.com>
2026-03-24 08:30:56 +0100

    add makeSUT helper

--- Core/CoreTests/Presentation/ProductsImagePresenterTests.swift
+++ Core/CoreTests/Presentation/ProductsImagePresenterTests.swift
@@ -5,4 +5,44 @@
 //  Created by Matteo Casu on 24/03/26.
 //
 
-import Foundation
+import XCTest
+
+
+class ProductImagePresenter {
+    
+    var view: Any
+    
+    init(view: Any) {
+        self.view = view
+    }
+}
+
+public final class ProductsImagePresenterTests: XCTestCase {
+    
+    
+    func test_init_doesNotSendMessagesToView() {
+        
+        let (_, spy) = makeSUT()
+        
+        XCTAssertTrue(spy.messages.isEmpty)
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ProductImagePresenter, spy: ViewSpy) {
+        
+        let spy = ViewSpy()
+        let sut = ProductImagePresenter(view: spy)
+        
+        trackForMemoryLeak(spy, file: file, line: line)
+        trackForMemoryLeak(sut, file: file, line: line)
+        
+        return (sut, spy)
+    }
+    
+    
+    private class ViewSpy {
+        var messages: [Any] = []
+    }
+}
